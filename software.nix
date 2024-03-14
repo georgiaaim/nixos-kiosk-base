04f7f8c267867e1cbcd4c3a2735851b012b145f0
Hunter <hdh20267@uga.edu>
2024-03-14 17:35:55 -0400

    new systemd services

--- software.nix
+++ software.nix
@@ -3,33 +3,41 @@ let
   home-assistant-qcow2 = pkgs.fetchurl {
     name = "home-assistant.qcow2";
     url = "https://github.com/home-assistant/operating-system/releases/download/12.1/haos_ova-12.1.qcow2.xz";
-    sha256 = "1rrhrzp5y219phd4iwj2ak2fvnrxdgjx7lszkp7pll97104fybff";
+    sha256 = "SIcYHisZieaKpZDqXGJc7pUM8c1E1EpVtnjSlvapS2g=";
     postFetch = ''
       cp $out src.xz
       ${pkgs.xz}/bin/unxz src.xz --stdout > $out
     '';
-  }
+  };
 in
 {
   services.kioskAdmin.enable = true;
   # Mount the qcow2 to /etc/homeassistant
   systemd.services.mount-home-assistant-qcow2 = {
     description = "Mount Home Assistant qcow2";
+
     wantedBy = [ "multi-user.target" ];
+
     after = [ "local-fs.target" ];
+
+    preStart = ''
+      if [ ! -f /etc/home-assistant.qcow2 ]; then
+        cp ${home-assistant-qcow2} /etc/home-assistant.qcow2
+      fi
+    '';
+
+    script = ''
+      ${pkgs.qemu}/bin/qemu-nbd --connect=/dev/nbd0 /etc/home-assistant.qcow2
+      mount /dev/nbd0p1 /etc/homeassistant
+    '';
+
+    preStop = ''
+      umount /etc/homeassistant
+      ${pkgs.qemu}/bin/qemu-nbd --disconnect /dev/nbd0
+    '';
+
     serviceConfig = {
       Type = "oneshot";
-      ExecStartPre = ''
-        if [ ! -f /etc/home-assistant.qcow2 ]; then
-          cp ${home-assistant-qcow2} /etc/home-assistant.qcow2
-        fi
-      '';
-      ExecStart = "${pkgs.qemu}/bin/qemu-nbd --connect=/dev/nbd0 /etc/home-assistant.qcow2";
-      ExecStartPost = "mount /dev/nbd0p1 /etc/homeassistant";
-      ExecStop = ''
-        umount /etc/homeassistant";
-        ExecStop = "${pkgs.qemu}/bin/qemu-nbd --disconnect /dev/nbd0
-      '';
     };
   };
 
