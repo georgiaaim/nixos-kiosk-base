edbbf767b178d882ded2701e9375ae606312fc5d
Hunter <hdh20267@uga.edu>
2024-03-28 11:41:44 -0400

    change format of dnsmasq config

--- configuration.nix
+++ configuration.nix
@@ -54,17 +54,16 @@
 
   services.dnsmasq = {
     enable = true;
-    settings = ''
-      interface=lan
-      except-interface=lo
-      log-dhcp
+    settings = {
+      interface=lan;
+      except-interface=lo;
 
-      dhcp-range=10.0.0.2,10.0.0.254,255.255.255.0,12h
-      dhcp-option=option:router,10.0.0.1
-      dhcp-option=option:dns-server,1.1.1.1
-      dhcp-option=option:broadcast-address,10.0.0.255
-      dhcp-option=option:subnet-mask,255.255.255.0
-    '';
+      dhcp-range=[ "10.0.0.2,10.0.0.254,255.255.255.0" ];
+      dhcp-option="option:router,10.0.0.1";
+      dhcp-option="option:dns-server,1.1.1.1";
+      dhcp-option="option:broadcast-address,10.0.0.255";
+      dhcp-option="option:subnet-mask,255.255.255.0";
+    };
   };
 
   #networking.defaultGateway = "10.0.0.1";
