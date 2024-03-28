d7471b34ae5fc6aacec167a7b93ab828f0d8ab45
Hunter <hdh20267@uga.edu>
2024-03-28 13:30:46 -0400

    dnsmasq update

--- configuration.nix
+++ configuration.nix
@@ -55,16 +55,19 @@
   services.dnsmasq = {
     enable = true;
     settings = {
-      interface="lan";
+      server = [ "1.1.1.1" "8.8.8.8" ];
+      no-resolv = true;
+      domain-needed = true;
+      except-interface="wan";
       except-interface="lo";
 
-      dhcp-range=[ "10.0.0.2,10.0.0.254,255.255.255.0" ];
-      dhcp-option= [ 
-        "option:router,10.0.0.1"
-        "option:dns-server,1.1.1.1"
-        "option:broadcast-address,10.0.0.255"
-        "option:subnet-mask,255.255.255.0"
-      ];
+      dhcp-range=[ "lan,10.0.0.2,10.0.0.254,24h" ];
+      interface="lan";
+      dhcp-host = "10.0.0.1";
+
+      local = "/lan/";
+      domain = "lan";
+      expand-hosts = true;
     };
   };
 
