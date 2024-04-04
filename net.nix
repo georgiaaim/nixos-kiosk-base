f57b5234fafeb088e4a507711b88cff6d2540793
Hunter <hdh20267@uga.edu>
2024-04-04 10:29:43 -0400

    tcp mdns firewall

--- net.nix
+++ net.nix
@@ -10,7 +10,7 @@
     firewall = {
       enable = true;
       # SSH, DNS, MQTT, Unifi, Home Assistant, and Unifi respectively
-      allowedTCPPorts = [ 22 53 1883 6053 8080 8123 8443 50537 ];
+      allowedTCPPorts = [ 22 53 1883 5353 6053 8080 8123 8443 50537 ];
 
       # DNS, DHCP, MQTT, Unifi, and mDNS respectively
       allowedUDPPorts = [ 53 67 1883 3478 5353 6053 50537 ];
