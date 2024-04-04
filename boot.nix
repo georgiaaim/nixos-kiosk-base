8bbeeb98f99b0572bf6834fef6096983390d2dcf
Hunter <hdh20267@uga.edu>
2024-04-04 08:51:25 -0400

    try gnome over plasma

--- boot.nix
+++ boot.nix
@@ -36,17 +36,15 @@
   system.stateVersion = "24.05";
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Enable Wayland and SDDM for Plasma 6
   services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.sddm.wayland.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
-  # Immediately open kiosk user on boot
-  #services.xserver.displayManager.autoLogin = {
-  #  enable = true;
-  #  user = "kiosk";
-  #};
+   Immediately open kiosk user on boot
+  services.xserver.displayManager.autoLogin = {
+    enable = true;
+    user = "kiosk";
+  };
 
   # Enable SSH for remote access
   services.openssh.enable = true;
