d294f5b96e71e49e0a91c98253f9e7ad27f8e31d
Hunter <hdh20267@uga.edu>
2024-05-16 11:18:19 -0400

    try to move the window

--- users/kiosk-home.nix
+++ users/kiosk-home.nix
@@ -5,10 +5,11 @@ let
     monitors=$(xrandr --query | grep " connected" | cut -d" " -f1)
     monitor_count=$(echo $monitors | wc -w)
     ${pkgs.firefox}/bin/firefox --kiosk http://homeassistant.local:8123 &
+    sleep 2
     if [ $monitor_count -gt 1 ]; then
       ${pkgs.firefox}/bin/firefox -new-window --kiosk http://homeassistant.local:8123 &
       window_ids=$(wmctrl -l | grep "Firefox" | awk '{print $1}')
-      sleep 10
+      sleep 2
       ${pkgs.wmctrl}/bin/wmctrl -i -r $(echo $window_ids | awk '{print $1}') -e 0,2560,720,1280,720
       ${pkgs.wmctrl}/bin/wmctrl -i -r $(echo $window_ids | awk '{print $2}') -e 0,0,0,1440,900
     fi
