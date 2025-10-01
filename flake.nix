aabedab32f7cd163718d17e244cc457b4d034ea9
Hunter <hdh20267@uga.edu>
2025-10-01 15:59:34 -0400

    lock to 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Base UGA Module Factory Kiosk Configuration for ZimaBoard";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
