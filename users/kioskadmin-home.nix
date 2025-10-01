0b136191216ed5be33862db49a14fae085dfe685
Hunter <hdh20267@uga.edu>
2025-10-01 15:55:35 -0400

    fix font update

--- users/kioskadmin-home.nix
+++ users/kioskadmin-home.nix
@@ -27,7 +27,7 @@
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
     # # fonts?
-    pkgs.nerd-fonts.meslo
+    pkgs.nerd-fonts.meslo-lg
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
