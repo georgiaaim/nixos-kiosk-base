5aecaa77b0a213116ec82fb97b1c102d2dedb0bb
Hunter <hdh20267@uga.edu>
2025-10-24 17:08:27 -0400

    fix hass hash

--- sw.nix
+++ sw.nix
@@ -4,7 +4,7 @@ let
   home-assistant-qcow2 = pkgs.fetchurl {
     name = "home-assistant.qcow2";
     url = "https://github.com/home-assistant/operating-system/releases/download/16.2/haos_ova-16.2.qcow2.xz";
-    sha256 = "sha256-PLOJ1WGCKVDDb9b0nLQeVyVM9J/esuFkSKeMphcucw=";
+    sha256 = "sha256-PLOJ1WGCKVDDb960nIQeVYVM9Jh7esuFkSKeMphcucw=";
     postFetch = ''
       cp $out src.xz
       ${pkgs.xz}/bin/unxz src.xz --stdout > $out
